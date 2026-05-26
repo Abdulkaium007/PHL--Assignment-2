f6662239ff8d0df6e15eef85151854ec67a837d9
Abdulkaium007 <smakaium6957@gmail.com>
2026-05-26 23:29:07 +0600

    update issue logic added

--- src/modules/issues/issuesController.ts
+++ src/modules/issues/issuesController.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { issuesService } from "./issuesService";
 import sendResponse from "../../utility/sendResponse";
 import { Result } from "pg";
+import { pool } from "../../db/index";
 
 
 const createIssue = async(req: Request, res: Response) => {
@@ -28,11 +29,39 @@ const createIssue = async(req: Request, res: Response) => {
 const getAllIssues = async(req: Request, res: Response) => {
     try {
         const result = await issuesService.getAllIssuesFromDB();
+        const allIssues = result.rows;
+
+        const reporterInfo = await pool.query(
+            `SELECT id, name, role FROM users`
+        );
+
+        const reporterMap = new Map();
+
+        reporterInfo.rows.map((reporter) => {
+            reporterMap.set(reporter.id, reporter);
+        });
+
+        const formattedIssues = allIssues.map((issue) => {
+            return {
+                id: issue.id,
+                title: issue.title,
+                description: issue.description,
+                type: issue.type,
+                status: issue.status,
+
+                reporter: reporterMap.get(issue.reporter_id),
+
+                created_at: issue.created_at,
+                updated_at: issue.updated_at,
+            };
+        });
+        // console.log(formattedIssues);
+
         sendResponse(res, {
             statusCode: 200,
             success: true,
             message: "Issues retrieved successfully!",
-            data: result.rows
+            data: formattedIssues
         });
     } catch (error: unknown) {
         const errorMessage = error instanceof Error ? error.message : "Failed to retrieve issues.";
@@ -58,11 +87,28 @@ const getSingleIssue = async(req: Request, res: Response) => {
             })
         }
 
+        const reporterInfo = await pool.query(
+            `SELECT id, name, role FROM users WHERE id = $1`, [result.rows[0].reporter_id],
+        );
+        // console.log(reporterInfo.rows[0]); 
+        const formatedIssue = {
+            id: result.rows[0].id,
+            title: result.rows[0].title,
+            description: result.rows[0].description,
+            type: result.rows[0].type,
+            status: result.rows[0].status,
+            reporter: reporterInfo.rows[0],
+            created_at: result.rows[0].created_at,
+            updated_at: result.rows[0].updated_at,
+
+        }
+        // console.log(formatedIssue);
+
         sendResponse(res, {
             statusCode: 200,
             success: true,
             message: "Issue retrieved successfully!",
-            data: result.rows[0]
+            data: formatedIssue
         });
     } catch (error: unknown) {
         const errorMessage = error instanceof Error ? error.message : "Failed to retrieve issue.";
@@ -77,9 +123,11 @@ const getSingleIssue = async(req: Request, res: Response) => {
 
 const updateIssue = async(req: Request, res: Response)=>{
     const {id} = req.params;
+    const loggedInUser = req.user;
+
     try{
-        const result = await issuesService.updateIssueInDB(id as string, req.body)
-        if (result.rows.length===0){
+        const existingIssue = await issuesService.getSingleIssueFromDB(id as string)
+        if (existingIssue.rows.length===0){
             sendResponse (res,{
                 statusCode: 404,
                 success: false,
@@ -87,6 +135,35 @@ const updateIssue = async(req: Request, res: Response)=>{
                 data: {}
             })
         }
+
+        if (loggedInUser?.role === "contributor") {
+
+            if (existingIssue.rows[0].reporter_id !== loggedInUser.id) {
+                return sendResponse(res, {
+                    statusCode: 403,
+                    success: false,
+                    message: "You can only update your own issue",
+                });
+            }
+
+            if (existingIssue.rows[0].status !== "open") {
+                return sendResponse(res, {
+                    statusCode: 409,
+                    success: false,
+                    message: "Issue already in progress or closed",
+                });
+            }
+            
+            if (req.body.status) {
+                return sendResponse(res, {
+                    statusCode: 403,
+                    success: false,
+                    message: "Contributors cannot change issue status",
+                });
+            }
+        }
+
+        const result = await issuesService.updateIssueInDB(id as string, req.body)
 
         sendResponse(res, {
             statusCode: 200,
--- src/modules/issues/issuesRoute.ts
+++ src/modules/issues/issuesRoute.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { issuesController } from "./issuesController";
+import auth from "../../middlewares/auth";
+import { USER_ROLE } from "../../types/userTypes";
 
 
 const router = Router();
 
-router.post("/", issuesController.createIssue);
-router.get("/", issuesController.getAllIssues);
-router.get("/:id", issuesController.getSingleIssue);
-router.put("/:id", issuesController.updateIssue);
-router.delete("/:id", issuesController.deleteIssue);
+router.post("/", auth(USER_ROLE.contributor, USER_ROLE.maintainer), issuesController.createIssue);
+router.get("/", auth(USER_ROLE.contributor, USER_ROLE.maintainer), issuesController.getAllIssues);
+router.get("/:id", auth(USER_ROLE.contributor, USER_ROLE.maintainer), issuesController.getSingleIssue);
+router.put("/:id", auth(USER_ROLE.contributor, USER_ROLE.maintainer), issuesController.updateIssue);
+router.delete("/:id", auth( USER_ROLE.maintainer), issuesController.deleteIssue);
 
 export const issuesRoute = router;
